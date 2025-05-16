6f9be0c917682cb82719f0e37ec29e80469dd47e
GraciaRosalinaHindirwan <rosalinagracia331@gmail.com>
2025-05-16 20:42:53 +0700

    update register - but not finished

--- register.php
+++ register.php
@@ -8,12 +8,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $confirm = ($_POST["confirm"]);
     $check = isset ($_POST["check"]);
 
-//simpan pesan error 
-    $error = []; 
-
  //validasi 
    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
     $_SESSION["login_error"] = "Format E-mail Tidak Valid";
+            require_once("route.php");
+            redirect("register.php");
    } if (strlen($username) < 3) {
     $_SESSION["login_error"] = "Username Minimal 3 Karakter";
    } if ($password != $confirm) {
@@ -37,16 +36,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
        $sql = "INSERT INTO user (email, username, password) VALUE (?, ?, ?)";
        $connection = getConnection();
        $stmt = $connection->prepare($sql); //prepared statement 
-       $stmt->execute([$email, $username, $password]);
-     
-       if ($stmt -> rowcount()) {
-        echo "Registrasi berhasil!";
-        } else {
-            echo "error";
-        }
-    }
 
-   exit();
+       try{
+        $stmt->execute([$email, $username, $password]);
+        if ($stmt -> rowcount()) {
+        $_SESSION["login_error"] = "Register Berhasil";
+        require_once("route.php");
+        redirect("login.php");
+        }
+       } catch(Exception $e){
+            $_SESSION["login_error"] = "Invalid Username or E-mail";
+            require_once("route.php");
+            redirect("register.php");
+       }
+    }
 }
 
 
@@ -67,34 +70,29 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <img src="img/header-regis.webp" class="card-img-top">
     <div class="card-body">
         <h5 class="card-title">REGISTER</h5>
-        <?php if (isset ($_SESSION["login_error"])) :?>
-        <?php $login_error = $_SESSION["login_error"];
-            unset($_SESSION["login_error"]);
-        ?>
-
 <form action="register.php" method="POST">
         <div class="card-text">
             <div class="form-floating">
-                <input type="email" class="form-control" id="floatingemail" placeholder="name@example.com" name="email">
+                <input type="email" class="form-control" id="floatingemail" placeholder="name@example.com" name="email" required>
                 <label for="floatingemail" class="bi bi-envelope"> Email</label>
             </div>
             <br>
             <div class="form-floating">
-                <input type="text" class="form-control" id="floatingUsername" placeholder="Username" name="username">
+                <input type="text" class="form-control" id="floatingUsername" placeholder="Username" name="username" required>
                 <label for="floatingUsername" class="bi bi-person"> Username</label>
             </div>
             <div class="row">
                 <div class="col">
                     <label for="formGroupExampleInput" class="bi bi-key"> Password</label>
-                    <input type="password" class="form-control" placeholder="Password" aria-label="Password" name="password">
+                    <input type="password" class="form-control" placeholder="Password" aria-label="Password" name="password" required>
                 </div>
                 <div class="col">
                     <label for="formGroupExampleInput" class="bi bi-key"> Confirm Password</label>
-                    <input type="password" class="form-control" placeholder="Confirm Password" aria-label="Confirm Password" name="confirm">
+                    <input type="password" class="form-control" placeholder="Confirm Password" aria-label="Confirm Password" name="confirm" required>
                 </div>
                 <div class="col-12">
                     <div class="form-check">
-                        <input class="form-check-input" type="checkbox" id="gridCheck" name="check">
+                        <input class="form-check-input" type="checkbox" id="gridCheck" name="check" required>
                         <label class="form-check-label" for="gridCheck">agree to the Terms & Condition</label>
                     </div>
                 </div>
@@ -107,6 +105,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 </form>
 
 <div class="info">
+     <?php if (isset ($_SESSION["login_error"])) :?>
+        <?php $login_error = $_SESSION["login_error"];
+            unset($_SESSION["login_error"]);
+        ?>
+        <p style = "color:red;"><?= $login_error ?></p>
+        <?php endif; ?>
+
     <h1>Hello,</h1> <br>
     <p>Hello client, welcome to registration page. 
         Please fill out the form on the side to get more complete features. 
